33ec0e72309585f39b82f41cd70468dae7f8db10
Vetal0205 <vetal0205merkulov@gmail.com>
2022-06-25 12:56:06 +0100

    Added db init cheker, added fetch from table func

--- db.py
+++ db.py
@@ -1,28 +1,35 @@
 import sqlite3
 from typing import Dict, List, Tuple, Optional
+from Exceptions import NoSQLSequence
 
 
 class Db:
     # SqlScript for db creation if not exists
-    def __init__(self, Connect: str, SqlScript: Optional[str]) -> None:
+    def __init__(self, Connect: str, SqlScript: Optional[str]):
         self.conn = sqlite3.connect(Connect)
         self.cursor = self.conn.cursor()
-        if SqlScript:
-            with open(SqlScript, "r") as f:
-                sql = f.read()
-            self.cursor.executescript(sql)
-            self.conn.commit()
+        self.cursor.execute("SELECT name FROM sqlite_master "
+                            "WHERE type='table' AND name='Task'")
+        table_exists = self.cursor.fetchall()
+        if not table_exists:
+            try:
+                self._init_db(SqlScript)
+            except NoSQLSequence():
+                pass
+
+    def _init_db(self, SqlScript: str) -> None:
+        with open(SqlScript, "r") as f:
+            sql = f.read()
+        self.cursor.executescript(sql)
+        self.conn.commit()
 
     def insert_columns(self, table: str, columns: Dict) -> None:
         keys = ",".join(columns.keys())
         values = [tuple(columns.values())]
         placeholders = ",".join("?" * len(columns.keys()))
         self.conn.executemany(
-            f"INSERT OR IGNORE INTO {table} "
-            f"({keys})"
-            f"({placeholders})",
-            values
-        )
+            f"INSERT INTO {table} ({keys}) VALUES ({placeholders})", values)
+        self.conn.commit()
 
     def get_cursor(self):
         return self.cursor
@@ -31,3 +38,9 @@ class Db:
         row_id = int(row_id)
         self.cursor.execute(f"DELETE FROM {table} WHERE id = ?", (row_id,))
         self.conn.commit()
+
+    def fetchall(self, table: str, columns: List[str]) -> List:
+        columns_joined = ", ".join(columns)
+        self.cursor.execute(f"SELECT {columns_joined} FROM {table}")
+        rows = self.cursor.fetchall()
+        return rows
